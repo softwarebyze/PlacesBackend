7dc71c7053cc3352168d9c1d3472f6913438f7d5
softwarebyze <zackebenfeld@gmail.com>
2023-07-25 16:49:29 -0400

    change host and port

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // Import the framework and instantiate it
-import Fastify from 'fastify'
+import Fastify from "fastify";
 const fastify = Fastify({
-  logger: true
-})
+  logger: true,
+});
+
+const port = process.env.PORT || 8080;
 
 // Declare a route
-fastify.get('/', async function handler (request, reply) {
-  return { hello: 'world' }
-})
+fastify.get("/", async function handler(request, reply) {
+  return { hello: "world" };
+});
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({ host: "0.0.0.0", port });
 } catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
+  fastify.log.error(err);
+  process.exit(1);
 }
